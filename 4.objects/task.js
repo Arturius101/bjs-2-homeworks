d23dca65854675d86bf7b43bad02e333eb7430db
Arturius101 <art-dzhikiya@yandex.ru>
2023-02-27 13:07:03 +0300

    task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
 function Student(name, gender, age) {
-  
+    let student1 = new Student("Петя", "Мужской", 22);
+    student1.setSubject("Физика");
+    student1.addMark(5);
+    student1.addMark(4);
+    student1.addMark(5);
+    
+    let student2 = new Student("Лиза", "Женский", 21);
+    student2.setSubject("Химия");
+    student2.addMark(2);
+    student2.addMark(3);
+    student2.addMark(2);
+    student2.exclude('Низкая оценка')
+    
+    console.log(student1); 
+    console.log(student2);
 }
 
-Student.prototype.setSubject = function (subjectName) {
+// Student.prototype.setSubject = function (subjectName) {
   
-}
+// }
 
-Student.prototype.addMarks = function (...marks) {
+// Student.prototype.addMarks = function (...marks) {
   
-}
+// }
 
-Student.prototype.getAverage = function () {
+// Student.prototype.getAverage = function () {
   
-}
+// }
 
-Student.prototype.exclude = function (reason) {
+// Student.prototype.exclude = function (reason) {
   
-}
+// }
