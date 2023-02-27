f176e40c88f3b6e6963b9fcc108bb38a2d106838
Arturius101 <art-dzhikiya@yandex.ru>
2023-02-27 18:16:05 +0300

    4.objects/task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,33 +1,41 @@
 function Student(name, gender, age) {
-    let student1 = new Student("Петя", "Мужской", 22);
-    student1.setSubject("Физика");
-    student1.addMark(5);
-    student1.addMark(4);
-    student1.addMark(5);
-    
-    let student2 = new Student("Лиза", "Женский", 21);
-    student2.setSubject("Химия");
-    student2.addMark(2);
-    student2.addMark(3);
-    student2.addMark(2);
-    student2.exclude('Низкая оценка')
-    
-    console.log(student1); 
-    console.log(student2);
+    this.name = name;
+    this.gender = gender;
+    this.age = age;    
 }
 
-// Student.prototype.setSubject = function (subjectName) {
+  Student.prototype.setSubject = function (subjectName) {
+    this.subject = subjectName;
+  }
   
-// }
+  Student.prototype.addMarks = function (...marks) {
+    (this.marks === undefined) ? this.marks = [...marks] : this.marks.push(...marks);
+  }
+  
+  Student.prototype.getAverage = function () {
+    let sum = 0;
+    this.marks.forEach(item => sum += item);
+    return sum / this.marks.length;
+  }
+  
+  Student.prototype.exclude = function (reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+  }
+  
+  let student1 = new Student("Петя", "Мужской", 22);
+  student1.setSubject("Физика");
+  student1.addMarks(5);
+  student1.addMarks(4);
+  student1.addMarks(5);
+  
+  let student2 = new Student("Лиза", "Женский", 21);
+  student2.setSubject("Химия");
+  student2.addMarks(2);
+  student2.addMarks(3);
+  student2.addMarks(2);
+  student2.exclude('Низкая оценка')
 
-// Student.prototype.addMarks = function (...marks) {
-  
-// }
-
-// Student.prototype.getAverage = function () {
-  
-// }
-
-// Student.prototype.exclude = function (reason) {
-  
-// }
+  console.log(student1); 
+  console.log(student2);
