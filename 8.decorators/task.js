542cb39dcacad7f6d25ac349221b9ad8d33b7eb0
Arturius101 <art-dzhikiya@yandex.ru>
2023-03-21 11:36:15 +0300

    task.js

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,35 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+    let cache = [];
+
+    function wrapper(...args) {
+        const hash = args.join(',');
+        let idx = cache.findIndex((item)=> item.hash === hash);
+        if (idx !== -1) { 
+            console.log("Из кэша: " + cache[idx].result);
+            return "Из кэша: " + cache[idx].result;
+        }
+
+        let result = func(...args);
+        cache.push( {hash, result} );
+        if (cache.length > 5) { 
+        cache.shift();
+        }
+        console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result;  
+    }
+    return wrapper;
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
-}
+    let timerId = null;
+    function wrapper (...args) {
+      if (timerId === null) {
+        func(...args);
+      }
+      clearTimeout(timerId);
+      timerId = setTimeout(() => timerId = null, delay);
+    }
+    return wrapper;
+} 
